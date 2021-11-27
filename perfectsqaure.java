2f0c89dd870c24c4f6ea036e1f9f12a3495d2f65
DeependraRao01 <87476279+DeependraRao01@users.noreply.github.com>
2021-11-27 19:42:27 +0530

    Add files via upload
    perfect sqaure

---
+++ perfectsqaure.java
@@ -0,0 +1,24 @@
+package deependra;
+import java.util.Scanner;
+
+public class perfectsqaure {
+
+	public static void main(String[] args) {
+
+	  Scanner sc=new Scanner(System.in);
+	  System.out.println("Enter a number");
+	  int number=sc.nextInt();
+	  double x=Math.sqrt(number);
+	  if(x==(int)x)
+	  {
+		System.out.println("Given number is perfect sqaure");
+	  }
+	  else
+	  {
+		System.out.println("Given number is not a perfect square");
+		
+	}
+
+	}
+}
+	
